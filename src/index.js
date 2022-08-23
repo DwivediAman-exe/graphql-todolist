88a9de901a37f8a63adca83afecd85e78ef69e6d
Aman Dwivedi <19dwivediaman@gmail.com>
2022-08-23 17:13:31 +0530

    setup done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {
+	ApolloClient,
+	InMemoryCache,
+	gql,
+	createHttpLink,
+} from '@apollo/client';
+
+const uri = 'darling-hermit-86.hasura.app/v1/graphql';
+const secret =
+	'i66GuFAqSZENP3tM16cB6liWytuqlzNBWbfMwB84YbR7ivWZ82bNA9kTHPBnNOMB';
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+	uri: `https://${uri}`,
+	headers: {
+		'x-hasura-admin-secret': secret,
+	},
+});
+
+const createApolloClient = () => {
+	return new ApolloClient({
+		cache: new InMemoryCache(),
+		link: httpLink,
+	});
+};
+
+const client = createApolloClient();
+
+client
+	.query({
+		query: gql`
+			query getTodo {
+				todos {
+					done
+					id
+					text
+				}
+			}
+		`,
+	})
+	.then((data) => console.log(data));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<>
+		<App />
+	</>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
