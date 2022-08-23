a147ee7a4fa0c6ed6f44427ff30b29b7e666aeaa
Aman Dwivedi <19dwivediaman@gmail.com>
2022-08-23 21:53:51 +0530

    done function complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,86 @@
+import { gql, useQuery, useMutation } from '@apollo/client';
+
+const GET_TODOS = gql`
+	query getTodo {
+		todos {
+			done
+			id
+			text
+		}
+	}
+`;
+
+const TOGGLE_TODO = gql`
+	mutation toggleTodo($id: uuid!, $done: Boolean!) {
+		update_todos(where: { id: { _eq: $id } }, _set: { done: $done }) {
+			returning {
+				done
+				id
+				text
+			}
+		}
+	}
+`;
+
 function App() {
-	return <div>app</div>;
+	const { data, loading, error } = useQuery(GET_TODOS);
+
+	const [toggleTodo] = useMutation(TOGGLE_TODO);
+
+	async function handleToggleTodo({ id, done }) {
+		console.log('called');
+		const data = await toggleTodo({
+			variables: {
+				id,
+				done: !done,
+			},
+		});
+		console.log(data);
+	}
+
+	if (loading) return <div>Loading Todos</div>;
+	if (error) return <div>Error fetching Todos</div>;
+	return (
+		<div className="vh-100 code flex flex-column items-center bg-pink white pa3 fl-1">
+			<h1 className="f2-1">
+				GraphQL Checklist{'    '}
+				<span role="img" aria-label="checkmark">
+					✔
+				</span>
+			</h1>
+
+			<form className="mb3">
+				<input
+					type="text"
+					placeholder="Write your todo"
+					className="pa2 f4 b--dashed"
+				/>
+				<button type="Submit" className="pa2 f4 bg-green">
+					Create
+				</button>
+			</form>
+
+			<div className="flex items-center justify-center flex-column">
+				{data.todos.map((todo) => (
+					<p
+						onDoubleClick={() => handleToggleTodo(todo)}
+						key={todo.id}
+					>
+						<span
+							className={`pointer list pa1 f3 ${
+								todo.done && 'strike'
+							}`}
+						>
+							{todo.text}
+						</span>
+						<button className="bg-transparent bn f4">
+							<span className="red"> &times;</span>
+						</button>
+					</p>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from './App';
 import {
 	ApolloClient,
 	InMemoryCache,
-	gql,
 	createHttpLink,
+	ApolloProvider,
 } from '@apollo/client';
 
-const uri = 'darling-hermit-86.hasura.app/v1/graphql';
-const secret =
+const hasura_uri = 'darling-hermit-86.hasura.app/v1/graphql';
+const hasura_secret =
 	'i66GuFAqSZENP3tM16cB6liWytuqlzNBWbfMwB84YbR7ivWZ82bNA9kTHPBnNOMB';
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
-	uri: `https://${uri}`,
+	uri: `https://${hasura_uri}`,
 	headers: {
-		'x-hasura-admin-secret': secret,
+		'x-hasura-admin-secret': hasura_secret,
 	},
 });
 
@@ -27,26 +27,11 @@ const createApolloClient = () => {
 		link: httpLink,
 	});
 };
-
 const client = createApolloClient();
-
-client
-	.query({
-		query: gql`
-			query getTodo {
-				todos {
-					done
-					id
-					text
-				}
-			}
-		`,
-	})
-	.then((data) => console.log(data));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<>
+	<ApolloProvider client={client}>
 		<App />
-	</>
+	</ApolloProvider>
 );
